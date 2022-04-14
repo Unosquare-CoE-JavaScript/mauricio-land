a33a62b8f20b121b7263731fbf19c784cd3dc351
Mauricio Landa <mauricio.landa@valtira.net>
2022-04-14 10:14:17 -0400

    docs: add css course exercises

---
+++ 1.1/Assignments/Assignment 5/shared.js
@@ -0,0 +1,34 @@
+let backdrop = document.querySelector(".backdrop");
+let modal = document.querySelector(".modal");
+let modalNoButton = document.querySelector(".modal__action--negative");
+let selectPlanButtons = document.querySelectorAll(".plan button");
+let toggleButton = document.querySelector(".toggle-button");
+let mobileNav = document.querySelector(".mobile-nav");
+
+for (let i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function() {
+    modal.classList.add("open");
+    backdrop.classList.add("open");
+  });
+}
+
+backdrop.addEventListener("click", function() {
+  mobileNav.classList.remove("open");
+  closeModal();
+});
+
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.remove("open");
+}
+
+toggleButton.addEventListener("click", function() {
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
+});
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# mauricio-land
+# mauricio-landa
+
+1.1: CSS - The Complete Guide 2022 Edition
+
+Topics: concepts and theory behind CSS, advanced CSS features, Flexbox, CSS Grid, CSS Variables or Sass.
